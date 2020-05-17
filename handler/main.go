6be46a9f5e41f4de83c719c5fb58e95306dc44fd
dezson <dezsonagy2@gmail.com>
2020-05-17 20:54:35 +0200

    Getting user presence skeleton function

--- handler/main.go
+++ handler/main.go
@@ -6,7 +6,10 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"net/http"
+	"net/url"
 	"os"
+	"strconv"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -15,22 +18,44 @@ import (
 type Response events.APIGatewayProxyResponse
 
 var (
-	ErrMissingEnv = errors.New("SLACK_USER_SECRET and SLACK_AUTH_TOKEN environment variables cannot be empty")
+	ErrMissingEnv     = errors.New("SLACK_USER_SECRET and SLACK_AUTH_TOKEN environment variables cannot be empty")
+	ErrNon200Response = errors.New("Non 200 response found")
 )
+
+func GetUserPresence(userSecret string, token string) error {
+	data := url.Values{}
+	data.Add("token", token)
+	data.Add("user", userSecret)
+	fmt.Printf("Request Arguments: %s\n", data.Encode())
+
+	client := &http.Client{}
+
+	req, _ := http.NewRequest("GET", "https://slack.com/api/users.getPresence", bytes.NewBufferString(data.Encode()))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Cache-Control", "no-cache")
+	req.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return ErrNon200Response
+	}
+
+	fmt.Printf("Response body: %s\n", resp.Body)
+	return nil
+}
 
 func Handler(ctx context.Context) (Response, error) {
 	fmt.Println("main.Handler")
 
 	slackUserSecret := os.Getenv("SLACK_USER_SECRET")
 	sleckToken := os.Getenv("SLACK_AUTH_TOKEN")
-	fmt.Printf("Configuration loaded: \n\t SLACK_USER_SECRET=%s \n\t SLACK_AUTH_TOKEN=%s", slackUserSecret, sleckToken)
+	fmt.Printf("Configuration loaded: \n\t SLACK_USER_SECRET=%s \n\t SLACK_AUTH_TOKEN=%s\n", slackUserSecret, sleckToken)
 
 	if len(slackUserSecret) == 0 || len(sleckToken) == 0 {
 		return Response{}, ErrMissingEnv
 	}
 
 	var buf bytes.Buffer
-
 	body, err := json.Marshal(map[string]interface{}{
 		"message": "Go Serverless v1.0! Your function executed successfully!",
 	})
